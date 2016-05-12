12d1fbbfd51f28882aaff10a05536afd7bda0510
davehrtng <david.e.harting@gmail.com>
2016-05-11 22:04:07 -0400

    customize view of Level to display image and look better

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160508172404) do
+ActiveRecord::Schema.define(version: 20160512003232) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,14 @@ ActiveRecord::Schema.define(version: 20160508172404) do
   end
 
   add_index "clicks", ["user_id"], name: "index_clicks_on_user_id", using: :btree
+
+  create_table "levels", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "number_of_clicks"
+    t.string   "image_name"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
