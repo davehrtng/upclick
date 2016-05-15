25eb52f586660ac431d8d385a74597c0e961be09
davehrtng <david.e.harting@gmail.com>
2016-05-15 13:12:29 -0400

    refactor upclick controller to accommodate gamer refactor

--- app/controllers/upclick_controller.rb
+++ app/controllers/upclick_controller.rb
@@ -2,7 +2,7 @@ class UpclickController < ApplicationController
   def index
     @total_click_count = Click.count
     if user_signed_in?
-      @user_click_count = current_user.clicks.count
+      @user_click_count = current_user.gamer.clicks.count
     end
   end
   
@@ -10,13 +10,13 @@ class UpclickController < ApplicationController
     newClickResponse = Hash.new
     
     if user_signed_in?
-      @click = current_user.clicks.build
+      @click = current_user.gamer.clicks.build
       @click.save
       
-      highest_level = Level.where("number_of_clicks <= ?", current_user.clicks.count).order("number_of_clicks DESC").first
+      highest_level = Level.where("number_of_clicks <= ?", current_user.gamer.clicks.count).order("number_of_clicks DESC").first
       
-      if highest_level && (current_user.level.nil? || current_user.level.number_of_clicks < highest_level.number_of_clicks)
-        current_user.level = highest_level
+      if highest_level && (current_user.gamer.level.nil? || current_user.gamer.level.number_of_clicks < highest_level.number_of_clicks)
+        current_user.gamer.level = highest_level
         current_user.save
         
         newLevelResponse = Hash.new
@@ -26,7 +26,7 @@ class UpclickController < ApplicationController
         
         newClickResponse["new_level"] = newLevelResponse 
       end
-      newClickResponse["user_clicks"] = current_user.clicks.count
+      newClickResponse["user_clicks"] = current_user.gamer.clicks.count
     else
       @click = Click.new # anonymous click
       @click.save
